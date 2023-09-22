0ee1d6eefef229f0601120528862e9924c3b8bfa
yiyanglin0102 <yiyanglin0102@gmail.com>
2023-09-22 16:30:51 -0400

    Completed 'Session_Creation_and_use' project

--- 12-Sept-2023/Session_Creation_and_use/public/app.js
+++ 12-Sept-2023/Session_Creation_and_use/public/app.js
@@ -3,42 +3,8 @@ const itemList = document.getElementById("item-list");
 const likedList = document.getElementById("liked-list");
 
 const addItemButton = document.getElementById("addItemButton");
-const getLikedButton = document.getElementById("getLiked");
-
-const testButton = document.getElementById("test");
 
 const buttonNames = [];
-
-testButton.addEventListener('click', () => {
-  const name = "test123"; // Get the name from the input field
-
-  // Create a JSON object with the name to send in the request body
-  const requestBody = {
-    name: name,
-  };
-
-  fetch('http://localhost:3000/like', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(requestBody), // Convert the object to JSON
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.data);
-    })
-    .catch((error) => console.error(error));
-});
-
-getLikedButton.addEventListener('click', () => {
-  fetch('http://localhost:3000/liked-baby')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.data);
-      })
-      .catch((error) => console.error(error));
-});
 
 // Add a click event listener to the "Add Item" button
 addItemButton.addEventListener("click", () => {
@@ -60,9 +26,45 @@ addItemButton.addEventListener("click", () => {
 
         // Add a click event listener to the new button
         newButton.addEventListener("click", () => {
+
+          const name = newItemValue; // Get the name from the input field
+
+          // Create a JSON object with the name to send in the request body
+          const requestBody = {
+            name: name,
+          };
+
+          fetch('http://localhost:3000/like', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody), // Convert the object to JSON
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data.data);
+            })
+            .catch((error) => console.error(error));
+
           alert(`Added Baby Name: ${newItemValue}`);
           buttonNames.push(newItemValue);
           console.log(buttonNames)
+          fetch('http://localhost:3000/liked-baby')
+            .then((res) => res.json())
+            .then((data) => {
+              const likedNames = data.likedNames;
+              // Clear the existing list
+              likedList.innerHTML = '';
+
+              // Create list items for each liked name and append them to the <ul>
+              likedNames.forEach((name) => {
+                const listItem = document.createElement('li');
+                listItem.textContent = name;
+                likedList.appendChild(listItem);
+              });
+            })
+            .catch((error) => console.error(error));
         });
       })
       .catch((error) => console.error(error));
--- 12-Sept-2023/Session_Creation_and_use/server.mjs
+++ 12-Sept-2023/Session_Creation_and_use/server.mjs
@@ -9,17 +9,59 @@ app.use(express.json()); // Add this middleware to parse JSON request bodies
 
 // Sample array of names
 const names = [
-  "Emily", "James", "Sophia", "William", "Olivia", "Benjamin", "Mia", "Daniel", "Ava", "Michael",
-  "Charlotte", "Elijah", "Amelia", "Alexander", "Harper", "Henry", "Evelyn", "Samuel", "Abigail", "Matthew",
-  "Elizabeth", "Joseph", "Sofia", "David", "Scarlett", "Christopher", "Grace", "Andrew", "Lily", "Jack",
-  "Victoria", "Noah", "Madison", "Olivia", "Liam", "Chloe", "Aiden", "Penelope", "Daniel", "Riley", "Carter",
-  "Layla", "William", "Zoey", "Owen", "Nora", "Ethan", "Lily", "Alexander", "Eleanor", "Sebastian", "Hazel",
-  "James", "Aria", "Benjamin", "Audrey", "Samuel", "Leah", "Joseph", "Grace", "Matthew", "Sophie", "John", "Zoe",
-  "David", "Mila", "Jackson", "Aubrey", "Luke", "Stella", "Christopher", "Hannah", "Daniel", "Maya", "Anthony",
-  "Savannah", "Gabriel", "Addison", "Nathan", "Ellie", "Dylan", "Claire", "Oliver", "Natalie", // Continue with more names...
-];
-let likedNames = ["Gabriel"];
+  "Melina", "Ayala", "Tanner", "Fleming", "Fatima", "Russell", "Weston", "Barry", "Waverly", "Nolan",
+  "Maximo", "Lawson", "Phoebe", "Ramirez", "David", "Wheeler", "Sydney", "Solis", "Ronin", "Hale",
+  "Brinley", "Moses", "Niklaus", "Hensley", "Malaya", "Merritt", "Colten", "Whitehead", "Sylvie", "Franco",
+  "Gage", "Vance", "Maxine", "Craig", "Odin", "Hunter", "Khloe", "Castillo", "Kai", "Anthony",
+  "Macy", "Franco", "Gage", "Beasley", "Jaylah", "Daugherty", "Turner", "Xiong", "Amayah", "Lester",
+  "Lee", "Shaffer", "Alanna", "Benitez", "Justice", "Donaldson", "Natasha", "Norman", "Aziel", "Peters",
+  "Leila", "Hobbs", "Brendan", "Montgomery", "Evangeline", "Floyd", "Pierce", "Mata", "Lillie", "Proctor",
+  "Vance", "Durham", "Tiffany", "Bradford", "Ander", "Serrano", "Allie", "Liu", "Pedro", "Wallace",
+  "Arianna", "Parra", "Davion", "Harper", "Ana", "Campos", "Gideon", "Reynolds", "Isabelle", "Melton",
+  "Lennon", "Duke", "Melani", "O'Neal", "Eddie", "Sanchez", "Aria", "Cantrell", "Harris", "Middleton",
+  "Madalyn", "Steele", "Elian", "Walls", "Lilianna", "Hess", "Lawrence", "Patterson", "Kaylee", "Becker",
+  "Lawson", "Conner", "Alondra", "Poole", "Quincy", "Mercado", "Mckinley", "Morrison", "Maximus", "Cervantes",
+  "Aylin", "Parra", "Davion", "Ingram", "Katie", "Duncan", "Avery", "Pennington", "Yareli", "Robertson",
+  "Emiliano", "Stanton", "Jaycee", "Bowers", "Dorian", "Rosales", "Kinley", "Tyler", "Emmitt", "Thornton",
+  "Haisley", "Wise", "Frederick", "Meyer", "Sara", "Powell", "Bennett", "Boyd", "Georgia", "Guerrero",
+  "Bryce", "Underwood", "Ensley", "Parsons", "Lewis", "Khan", "Mabel", "Gutierrez", "Luca", "Thompson",
+  "Madison", "Huber", "Mac", "Caldwell", "Evelynn", "Frederick", "Kase", "Nolan", "Itzayana", "Sims",
+  "Brian", "Moore", "Emily", "Deleon", "Nasir", "Nunez", "Mya", "Archer", "Ephraim", "Merritt", "Kaisley", "Lewis",
+  "Wyatt", "Hurley", "Rylan", "Yates", "Braylon", "Phelps", "Laney", "Russo", "Jamie", "Vaughn", "Reign", "Rowe",
+  "Kamden", "Chan", "Hattie", "Frye", "Franco", "Frederick", "Sariyah", "McFarland", "Dane", "Hobbs", "Lacey", "Lugo",
+  "Santos", "Briggs", "Alia", "Bartlett", "Kace", "Bullock", "Winnie", "Villa", "Clay", "Brooks", "Autumn", "McIntyre",
+  "Eliseo", "Velazquez", "Jaliyah", "French", "Corey", "Donovan", "Azariah", "Pearson", "Gunner", "English", "Kelly", "Jimenez",
+  "Silas", "Townsend", "Azalea", "Mayo", "Jericho", "Barnett", "Harlow", "Wong", "Walter", "Hanson", "Mariana", "Joseph",
+  "Kyle", "Church", "Ayleen", "Wilcox", "Jerry", "McKee", "Kori", "Kline", "Ramon", "Melton", "Kamiyah", "Burke", "Jax", "Savage",
+  "Louise", "Cunningham", "Alejandro", "Duncan", "Elise", "Palacios", "Thaddeus", "Frank", "Dior", "Rivas", "Damon", "George",
+  "Adelyn", "Clay", "Yosef", "Duran", "Willa", "Mata", "Ray", "Robles", "Felicity", "Cantu", "Anakin", "Walker", "Hazel", "Cannon",
+  "Archie", "Lane", "Amy", "Ward", "Jameson", "Bernal", "Emmeline", "Cline", "Cullen", "Baker", "Isla", "Tran", "Braxton", "Guerrero",
+  "Margot", "Moses", "Niklaus", "Gordon", "Taylor", "Russo", "Jamie", "Mata", "Lillie", "Enriquez", "Elisha", "McKenzie", "Briar", "Huff",
+  "Finnley", "Smith", "Olivia", "McGuire", "Casey", "Kelly", "Ruby", "Morales", "Aaron", "Cameron", "Julie", "Person", "Moses", "Davis",
+  "Mia", "Donaldson", "Canaan", "Nichols", "Aliyah", "Pacheco", "Erik", "Horn", "Avah", "Clark", "John", "Dennis", "Maisie", "Robles",
+  "Otto", "Schmitt", "Queen", "Kennedy", "Maxwell", "Schmidt", "Kimberly", "Mora", "Arturo", "Mitchell", "Willow", "Flores", "Lincoln", "Rhodes",
+  "Tatum", "Randall", "Trenton", "Harper", "Ana", "Rhodes", "Titus", "Strong", "Margo", "Fowler", "Kameron", "Arellano", "Faye", "Carey",
+  "Watson", "Logan", "Kora", "Wagner", "Enzo", "Copeland", "Dayana", "Villanueva", "Huxley", "Hail", "Lainey", "Logan", "Rocco", "Hess",
+  "Kaliyah", "Fitzpatrick", "Blaze", "Serrano", "Allie", "Hoffman", "Steven", "Keith", "Elyse", "Reed", "Easton", "Medina", "Elliana", "Calderon",
+  "Oakley", "Rollins", "Araceli", "Sharp", "Royce", "Jefferson", "Julieta", "Cox", "Connor", "Levy", "Flora", "Mosley", "Rayden", "Lewis",
+  "Ellie", "Osborne", "Augustus", "Byrd", "Giselle", "Coffey", "Kody", "Waller", "Whitley", "Gordon", "Karter", "Bernal", "Emmeline", "Page",
+  "Pablo", "Simmons", "Reagan", "Lyons", "Cyrus", "Payne", "London", "Adkins", "Kylo", "Murphy", "Bella", "Morgan", "Hunter", "Cherry",
+  "Nyomi", "Wilcox", "Jerry", "Edwards", "Ivy", "Powell", "Bennett", "Ochoa", "Luciana", "Luna", "Erick", "Bernal", "Emmeline", "Quintero",
+  "Thatcher", "Michael", "Aubriella", "Allen", "Carter", "Avery", "Meghan", "Lamb", "Kaysen", "Dominguez", "Raegan", "Hart", "Joel", "Massey",
+  "Clementine", "Andersen", "Alistair", "Kelley", "Rosalie", "Jennings", "Corbin", "Brandt", "Loretta", "Brewer", "Cruz", "Richmond",
+  "Whitney", "Berg", "Cayson", "Gonzalez", "Abigail", "Weaver", "Tucker", "Esquivel", "Jaylee", "Moran", "Tate", "Hammond", "Holly", "Fitzpatrick",
+  "Blaze", "Wells", "Cecilia", "Meyers", "Julien", "Chen", "Valeria", "Zamora", "Quentin", "Brewer", "Thea", "Porter", "Rhett", "Goodwin",
+  "Shiloh", "Ibarra", "Asa", "Buck", "Livia", "Taylor", "Jackson", "Page", "Cataleya", "Moyer", "Ahmir", "Oliver", "Camille", "Rojas",
+  "Colin", "Jenkins", "Rylee", "Gomez", "Isaiah", "Ryan", "Morgan", "Conley", "Marvin", "Watson", "Hailey", "Bentley", "Randy", "Clarke",
+  "Kaitlyn", "Clark", "John", "Cordova", "Florence", "Hebert", "Guillermo", "Landry", "Brynleigh", "Underwood", "Reece", "Russo",
+  "Tinsley", "Arnold", "Abraham", "Dillon", "Laurel", "Schroeder", "Izaiah", "Huffman", "Hayley", "Potts", "Alfred", "Vang", "Madisyn", "Hurst",
+  "Neil", "Bell", "Melody", "Peters", "Patrick", "Farley", "Wrenley", "Lamb", "Kaysen", "Contreras", "Daniela", "Wyatt", "Sam", "Hopkins",
+  "Gabriela", "Lee", "Jack", "Rojas", "Adaline", "Peters", "Patrick", "Kramer", "Hanna", "Perry", "Waylon", "Trevino", "Priscilla", "Barron",
+  "Dustin", "Bauer", "Haley", "English", "Junior", "Travis", "Mazikee", "Bryant", "Jonah", "Stevenson", "Regina", "Hunter", "Archer", "Burch",
+  "Freyja", "Cruz", "Ryan", "Harrington", "Legacy", "Parker", "Caleb", "Mathews", "Sloan"
+]
 
+let likedNames = [];
 
 // Function to get a random name from the array
 function getRandomName() {
@@ -32,21 +74,15 @@ app.get('/random-baby', (req, res) => {
   res.json({ data: randomBaby });
 });
 
-
 app.get('/liked-baby', (req, res) => {
-  const randomBaby = getRandomName();
-  res.json({ data: likedNames[0] });
+  res.json({ likedNames: likedNames }); // Respond with the entire likedNames array
 });
-
-
 app.post('/like', (req, res) => {
   const { name } = req.body; // Get the name from the request body
   likedNames.push(name); // Add the name to the likedNames array
   console.log(likedNames);
   res.json({ data: likedNames }); // Respond with the updated likedNames array
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
