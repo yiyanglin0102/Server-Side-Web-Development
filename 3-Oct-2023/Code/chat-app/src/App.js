2898f24d9da0cd6c2006df83b8805e60ec633197
yiyanglin0102 <yiyanglin0102@gmail.com>
2023-10-11 22:37:02 -0400

    Created 3-Oct-2023 for react and server for implementing Socket.io

---
+++ 3-Oct-2023/Code/chat-app/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Chat from './Chat';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Chat />
+      </header>
+    </div>
+  );
+}
+
+export default App;
